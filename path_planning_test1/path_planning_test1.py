11c82af5318dc73c5db1f69ff1c3218dfcd43bad
Luis <luis@accelone.com>
2018-02-23 19:43:44 -0300

    Added directions

--- path_planning_test1/path_planning_test1.py
+++ path_planning_test1/path_planning_test1.py
@@ -41,10 +41,13 @@ def print_matrix(matrix):
 
 def search():
     # Buffer to mark closed nodes
-
+    
+    # Init expand, closed and directions and directions_mark matrices
     node_index = 0
     expand = []
     closed = []
+    directions = []
+    directions_marks = []
 
     for row in range(len(grid)):
         expand_row = []
@@ -57,6 +60,18 @@ def search():
         for col in range(len(grid[0])):
             closed_row.append(0)
         closed.append(closed_row)
+
+    for row in range(len(grid)):
+        directions_row = []
+        for col in range(len(grid[0])):
+            directions_row.append(" ")
+        directions.append(directions_row)
+
+    for row in range(len(grid)):
+        directions_marks_row = []
+        for col in range(len(grid[0])):
+            directions_marks_row.append(0)
+        directions_marks.append(directions_marks_row)
 
     # Mark initial point as closed    
     closed[init[0]][init[1]] = 1
@@ -95,6 +110,7 @@ def search():
             # Check if next node is the goal
             if row == goal[0] and col == goal[1]:
                 found = True
+                directions[row][col] = "*"
                 print(next)
             # It is not the goal then evaluate movements
             else:
@@ -111,6 +127,7 @@ def search():
                             next_step = [g_inc, row_inc, col_inc]
                             open.append(next_step)
                             node_index += 1
+                            directions[row][col] = delta_name[i]
                             expand[row_inc][col_inc] = node_index
                             closed[row_inc][col_inc] = 1
 
@@ -120,8 +137,36 @@ def search():
             if expand[row][col] == 0 and not (row == init[0] and col == init[1]):
                 expand[row][col] = -1
 
-    print_matrix("Expand matrix")
+    # Analyze directions to get clean final path
+    found = False
+    row_cur = init[0]
+    col_cur = init[1]
+
+    while not found:
+        if row_cur == goal[0] and col_cur == goal[1]:
+            found = True
+        directions_marks[row_cur][col_cur] = 1
+        if directions[row_cur][col_cur] == '>':
+            col_cur += 1
+        elif directions[row_cur][col_cur] == '<':
+            col_cur -= 1
+        elif directions[row_cur][col_cur] == '^':
+            row_cur -= 1
+        elif directions[row_cur][col_cur] == 'v':
+            row_cur += 1
+
+    for row in range(len(directions)):
+        for col in range(len(directions[0])):
+            if directions_marks[row][col] == 0:
+                directions[row][col] = ' '
+
+    print("-------------")
+    print("Expand matrix")
     print_matrix(expand)
+
+    print("-------------")
+    print("Directions matrix")
+    print_matrix(directions)
 
 search()
 
