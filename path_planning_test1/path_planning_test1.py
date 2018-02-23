4f15aff5e0adea021d991b2048eecee8b70c6bad
Luis <luis@accelone.com>
2018-02-23 10:03:05 -0300

    recursive function WIP

--- path_planning_test1/path_planning_test1.py
+++ path_planning_test1/path_planning_test1.py
@@ -42,12 +42,14 @@ def search(grid,init,goal,cost):
     # insert code here
     # ----------------------------------------
     
-    print_matrix(grid)
     print(path)
-    input("Press key...")
+    # input("Press key...")
     # Explore available cells with provided cost
     grid[init[0]][init[1]] = 1
     previous_cost = 0
+    if len(path) == 0:
+        path.append([init[0], init[1], cost])
+
     for delta_step in delta:
         inc_row = init[0] + delta_step[0];
         inc_col = init[1] + delta_step[1];
@@ -57,9 +59,12 @@ def search(grid,init,goal,cost):
                 if inc_col == path_step[1]:
                     previous_cost = path_step[2]
 
-        if inc_row >= 0 and inc_col >= 0:
+        if inc_row >= 0 and inc_col >= 0 and inc_row < len(grid) and inc_col < len(grid[0]):
             if grid[inc_row][inc_col] == 0:
                 path.append([inc_row, inc_col, cost + previous_cost])
+                grid[inc_row][inc_col] == 1
+                print_matrix(grid)
+
     for path_step in path:
         row = path_step[0]
         col = path_step[1]
@@ -68,8 +73,6 @@ def search(grid,init,goal,cost):
             init[0] = row
             init[1] = col
             search(grid, init, goal, cost)  
-        else:
-            return path    
     
     return path
 
