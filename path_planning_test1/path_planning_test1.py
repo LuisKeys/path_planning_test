9cbb8df450f5b4883b5baccc17a1d4f0f7cb4292
Luis <luis@accelone.com>
2018-02-23 18:22:56 -0300

    working version

--- path_planning_test1/path_planning_test1.py
+++ path_planning_test1/path_planning_test1.py
@@ -20,10 +20,13 @@ grid = [[0, 0, 1, 0, 0, 0],
         [0, 0, 0, 0, 1, 0],
         [0, 0, 1, 1, 1, 0],
         [0, 0, 0, 0, 1, 0]]
+
+# Initial point, goal and cost
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
 
+# possible movements
 delta = [[-1, 0], # go up
          [ 0,-1], # go left
          [ 1, 0], # go down
@@ -31,51 +34,70 @@ delta = [[-1, 0], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
-path = []
-
+# Handy function to display a matrix
 def print_matrix(matrix):
-    for row in range(len(matrix) - 1):
+    for row in range(len(matrix)):
         print(matrix[row])
 
-def search(grid,init,goal,cost):
-    # ----------------------------------------
-    # insert code here
-    # ----------------------------------------
-    
-    print(path)
-    # input("Press key...")
-    # Explore available cells with provided cost
-    grid[init[0]][init[1]] = 1
-    previous_cost = 0
-    # Initialize path
-    if len(path) == 0:
-        path.append([init[0], init[1], cost])
-    
-    # Explore the 4 directions
-    for delta_step in delta:
-        inc_row = init[0] + delta_step[0];
-        inc_col = init[1] + delta_step[1];
-        # Get original initial cost
-        for path_step in path:
-            if inc_row == path_step[0]:
-                if inc_col == path_step[1]:
-                    previous_cost = path_step[2]
+def search():
+    # Buffer to mark closed nodes
+    closed = [[0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0]]
 
-        if inc_row >= 0 and inc_col >= 0 and inc_row < len(grid) and inc_col < len(grid[0]):
-            if grid[inc_row][inc_col] == 0:
-                path.append([inc_row, inc_col, cost + previous_cost])
-                grid[inc_row][inc_col] == 1
-                print_matrix(grid)
+    # Mark initial point as closed    
+    closed[init[0]][init[1]] = 1
 
-    for path_step in path:
-        row = path_step[0]
-        col = path_step[1]
-        g = path_step[2]
-        if grid[row][col] == 0:
-            init[0] = row
-            init[1] = col
-            search(grid, init, goal, cost)  
-    
-    return path
+    x = init[0]
+    y = init[1]
+    g = 0
 
-search(grid, init, goal, cost)
+    # Init open list
+    open = [[g, x, y]]
+
+    # process flags
+    found = False
+    resign = False
+
+    # print('Initial open list')
+    # print_matrix(open)
+
+    # Main iteration
+    while found is False and resign is False:    
+        # Empty open list means no solution
+        if len(open) == 0:
+            resign = True
+            print('fail')
+        #  Else continue search
+        else:
+            # Remove minor g value node from list
+            open.sort()
+            open.reverse()
+            next = open.pop()
+
+            x = next[1]
+            y = next[2]
+            g = next[0]
+
+            # Check if next node is the goal
+            if x == goal[0] and y == goal[1]:
+                found = True
+                print(next)
+            # It is not the goal then evaluate movements
+            else:
+                # Explore winning node, try to move on all possible directions
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    # Only add nodes within the grid
+                    if x2 >= 0 and y2 >= 0 and x2 < len(grid) and y2 < len(grid[0]):
+                        # Only add nodes on available locations (open and no obstacle)
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            # Update previous cost
+                            g2 = g + cost
+                            open.append([g2, x2, y2])
+                            closed[x2][y2] = 1
+
+search()
